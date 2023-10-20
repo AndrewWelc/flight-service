b05a781cfb228f85deeed0763eda0101654b7be8
Michal Marciniak <endriuwelc@gmail.com>
2023-10-20 21:01:18 +0200

    feat: add configService

--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -6,6 +6,7 @@ import {
   Inject,
   Logger,
 } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import axios from "axios";
 import { Cache } from "cache-manager";
 import { FlightDto } from "./dto/flight.dto";
@@ -14,43 +15,40 @@ import { FlightDto } from "./dto/flight.dto";
 export class FlightService {
   private readonly logger = new Logger(FlightService.name);
 
-  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache, private configService: ConfigService) {}
 
   async fetchFlights(): Promise<FlightDto[]> {
+    const source1Url = this.configService.get<string>('SOURCE1_URL');
+    const source2Url = this.configService.get<string>('SOURCE2_URL');
+    const cacheKey = this.configService.get<string>('CACHE_KEY_FLIGHTS');
+    const cacheExpiry = this.configService.get<string>('CACHE_EXPIRY_TIME');
+
     this.logger.log("Fetching flights");
 
-    const cachedFlights: FlightDto[] = await this.cacheManager.get("flights");
+    let cachedFlights = await this.cacheManager.get<FlightDto[]>(cacheKey);
     if (cachedFlights) {
       this.logger.log("Flights retrieved from cache");
       return cachedFlights;
     }
 
     try {
-      const source1Promise = axios.get(
-        "https://coding-challenge.powerus.de/flight/source1"
-      );
-      const source2Promise = axios.get(
-        "https://coding-challenge.powerus.de/flight/source2"
-      );
+      const source1Promise = axios.get(source1Url);
+      const source2Promise = axios.get(source2Url);
       const [source1, source2] = await Promise.all([
         source1Promise,
         source2Promise,
       ]);
 
-      const allFlights = [...source1.data.flights, ...source2.data.flights];
+      const allFlights: FlightDto[] = [...source1.data.flights, ...source2.data.flights];
       const uniqueFlights: FlightDto[] = this.removeDuplicates(allFlights);
 
-      await this.cacheManager.set("flights", uniqueFlights, 3600); // Cache for 1 hour
+      await this.cacheManager.set(cacheKey, uniqueFlights, { ttl: Number(cacheExpiry) });
 
       this.logger.log("Flights fetched and cached");
-
       return uniqueFlights;
     } catch (error) {
       this.logger.error("Failed to fetch flight data", error.stack);
-      throw new HttpException(
-        "Failed to fetch flight data",
-        HttpStatus.INTERNAL_SERVER_ERROR
-      );
+      throw new HttpException("Failed to fetch flight data", HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
